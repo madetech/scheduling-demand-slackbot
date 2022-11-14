f5b18339c326b1e1b44f550c9274543f23150ca8
agnieszkazbikowska-madetech <agnieszka.zbikowska@madetech.com>
2022-11-14 15:47:48 +0000

    Refactor the code with more meaningful names
    Co-authored-by: Korinna Pinakoulakis <kpinakoulaki@gmail.com>

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-function start() {
-  const data = getDataFromSpreadsheet();
-  const payload = buildPayload(data);
-  payload.forEach((message) => {
+function sendAllMessages() {
+  const spreadsheetContent = getDataFromSpreadsheet();
+  const messages = createMessages(spreadsheetContent);
+  messages.forEach((message) => {
     sendSlackMessage(message);
   })
 }
 
 function getDataFromSpreadsheet() {
-  const spreadsheet = SpreadsheetApp.getActive();
-  const firstSheet = spreadsheet.getSheets()[0];
+  const activeSpreadsheet = SpreadsheetApp.getActive();
+  const firstSheet = activeSpreadsheet.getSheets()[0];
   const range = firstSheet.getDataRange();
   const values = range.getValues();
   return values;
 }
 
-function buildPayload(data) {
+function createMessages(data) {
 
   const schedulingDemands = data.slice(1).map(column => {
     const account = column[0];
@@ -22,49 +22,43 @@ function buildPayload(data) {
     const start_date = column[2].toLocaleDateString();
     const end_date = column[3].toLocaleDateString();
 
-    const message = `*${assignment_demand}*\n${account}\n${start_date} to ${end_date}`
+    const message = `*${assignment_demand}* \n${account}\n${start_date} to ${end_date}`
     return message
   });
 
-  const payload_header = {
-    "blocks": [
-      {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": ":bell: *Hello here are the latest billable roles we are looking to fill:* :bell:"
-        }
-      }
-    ]
-  };
-
-  let payload_messages = [payload_header]
-
-  schedulingDemands.forEach((demand) => {
-    payload_messages.push({
+  const buildBlock = text => {
+    return {
       "blocks": [
         {
           "type": "section",
           "text": {
             "type": "mrkdwn",
-            "text": demand
+            "text": text
           }
         }
       ]
-    });
+    }
+  }
+
+  const payload_header = buildBlock(":bell: *Hello here are the latest billable roles we are looking to fill:* :bell:");
+
+  let messages = [payload_header]
+
+  schedulingDemands.forEach((demand) => {
+    messages.push(buildBlock(demand));
   })
 
-  return payload_messages;
+  return messages;
 }
 
-function sendSlackMessage(payload) {
-  var webhookUrl = ScriptProperties.getProperty('WEBHOOK_URL');
+function sendSlackMessage(message) {
+  const webhookUrl = ScriptProperties.getProperty('WEBHOOK_URL');
 
   const options = {
     "method": "post",
     "contentType": "application/json",
     "muteHttpExceptions": true,
-    "payload": JSON.stringify(payload)
+    "payload": JSON.stringify(message)
   };
 
   try {
